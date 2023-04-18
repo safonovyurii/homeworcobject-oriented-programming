f1c06815e9b363209402039ce5172d5e12828b89
safonov yurii <safonov-yurii89@mail.ru>
2023-04-18 19:18:47 +0700

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+
+    public String name;
+    public String pssport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
